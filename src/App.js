4610386f13c805da3127b5c67d74dcd8d2a81e07
james4-daf <d.james@elsevier.com>
2023-02-02 21:06:00 +0000

    api set up, displaying countries, flag in a card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        "https://ih-countries-api.herokuapp.com/countries"
+      );
+      setCountries(response.data);
+    }
+    fetchData();
+  }, []);
+  if (countries) {
+    return (
+      <div>
+        <Navbar />
+        {countries.map((country, i) => {
+          return (
+            <div key={`${country.name.common}${i}`}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="20"
+                  sx={{ width: 20 }}
+                  image={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                  alt="flag"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {country.name.common}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;

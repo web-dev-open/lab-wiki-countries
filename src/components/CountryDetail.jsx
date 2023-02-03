e499a834cd21c7803f78848af3a8a92be0577fd3
james4-daf <d.james@elsevier.com>
2023-02-03 13:44:10 +0000

    progress but not updating the url

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,7 +1,9 @@
 import Grid from "@mui/material/Grid";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, Route, Routes, useParams } from "react-router-dom";
+var countries = require("i18n-iso-countries");
+countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 function CountryDetail() {
   const { code } = useParams();
@@ -16,6 +18,7 @@ function CountryDetail() {
         name: { common },
         capital,
         alpha2Code,
+        alpha3Code,
         area,
         borders,
       } = response.data;
@@ -23,36 +26,48 @@ function CountryDetail() {
         common,
         capital,
         alpha2Code,
+        alpha3Code,
         area,
         borders,
       };
       setCountryDetail(details);
     }
     fetchCountries();
+
+    // console.log(countries.getName(countryDetail.alpha2Code, "en"));
   }, [code]);
 
   if (countryDetail) {
     return (
       <div>
-        <Grid container spacing={2}>
+        <Grid container spacing={4}>
           <Grid item xs={6}>
             <img
               alt="flag"
               src={`https://flagpedia.net/data/flags/icon/72x54/${countryDetail.alpha2Code.toLowerCase()}.png`}
             />
             <h1>{countryDetail.common}</h1>
+            {console.log(countries.getName(countryDetail.alpha2Code, "en"))}
+
             <p>Capital: {countryDetail.capital}</p>
             <p>Area: {countryDetail.area} km 2</p>
+
             <p>Borders:</p>
-            {countryDetail.borders.map((border, i) => {
+            {countryDetail.borders.map((border, code) => {
               return (
-                <div key={i}>
-                  <li>{border}</li>
+                <div key={code}>
+                  <Link to={`/country/${countryDetail.alpha3Code}`}>
+                    {countries.getName(border, "en")}
+                    {console.log(countryDetail.alpha3Code)}
+                  </Link>
                 </div>
               );
             })}
           </Grid>
         </Grid>
+        <Routes>
+          <Route path="/country/:code" element={<CountryDetail />} />
+        </Routes>
       </div>
     );
   }
